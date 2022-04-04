2cd3567ddf25aef026bb02911024e9d558edcd86
Maiara Aparecida Da Silva Ferreira <80417551+maiaraf@users.noreply.github.com>
2022-04-04 16:24:30 -0300

    Delete script.js

--- aula12exercicios.html/script.js
+++
@@ -1,7 +0,0 @@
-var hora = new Date().toLocaleTimeString('pt-BR');
- document.getElementById("horas").innerHTML = `Agora são ${hora} horas.`;
- document.getElementById("imagem").innerHTML = "<img src='https://images.pexels.com/photos/4448846/pexels-photo-4448846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' class='imagemStyle'>"
-
- if (hora <= 6) {
-     
- }
